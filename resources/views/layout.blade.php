8363d3a394b6364a661db17ee15cfa99db772847
Vicky AS <vickyhuang808@gmail.com>
2023-07-11 21:20:51 +0700

    reset favicon

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -5,7 +5,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>{{ $title }}</title>
-    {{-- <link rel="shortcut icon" href="{{ asset('public/favicon.ico') }}" type="image/x-icon"> --}}
     @vite('resources/css/app.css')
 </head>
 <body>
